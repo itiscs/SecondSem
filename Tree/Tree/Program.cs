267cacea569a39a164f2b3325db528b4b27787ae
moudroff <moudroff>
2018-03-14 13:22:53 +0300

    CreateTree

--- Tree/Tree/Program.cs
+++ Tree/Tree/Program.cs
@@ -37,12 +37,6 @@ namespace Tree
             ShowElem(Root);
         }
 
-
-
-        public static Tree<T> CreateTree(string str)
-        {
-            return null;
-        }
         
     }
 
@@ -75,6 +69,7 @@ namespace Tree
         public void ShowTree()
         {
             ShowElem(Root);
+            Console.WriteLine();
         }
 
         private int SumElem(Elem t)
@@ -90,7 +85,38 @@ namespace Tree
         {
             return SumElem(Root);
         }
-        
+
+        private Elem CreateElem(string[] str, ref int k)
+        {
+
+            if (!str[k].Contains('('))
+            {
+                return new Elem() { Info = int.Parse(str[k]) };
+            }
+            else
+            {
+                Elem t = new Elem();
+                t.Info = int.Parse(str[k].Remove(0, 1));//убрали '(' 
+                k++;
+                if (str[k] != "")
+                    t.Left = CreateElem(str, ref k);
+                k++;
+                if (str[k] != "")
+                    t.Right = CreateElem(str, ref k);
+                k++;
+                return t;
+            }
+            
+        }
+
+        public void CreateTree(string inputStr)
+        {
+            String[] str = inputStr.Split(new char[] { ',', ')' });
+            int k = 0;
+
+            Root = CreateElem(str,ref k);
+        }
+
     }
 
 
@@ -100,53 +126,61 @@ namespace Tree
     {
         static void Main(string[] args)
         {
-            Tree<int> t = new Tree<int>();
+            //Tree<int> t = new Tree<int>();
 
-            t.Root = new Elem<int>()
-            {
-                Info = 1,
-                Left = new Elem<int>() { Info = 2,
-                    Left  = new Elem<int>() { Info = 4},
-                    Right = new Elem<int>() { Info = 5}
-                },
-                Right = new Elem<int>() { Info = 3,
-                    //Left  = new Elem<int>() { Info = 6 },
-                    Right = new Elem<int>() { Info = 7,
-                        Left  = new Elem<int>() { Info = 8 },
-                        Right = new Elem<int>() { Info = 9 }
-                    }
-                }
-            };
+            //t.Root = new Elem<int>()
+            //{
+            //    Info = 1,
+            //    Left = new Elem<int>() { Info = 2,
+            //        Left  = new Elem<int>() { Info = 4},
+            //        Right = new Elem<int>() { Info = 5}
+            //    },
+            //    Right = new Elem<int>() { Info = 3,
+            //        //Left  = new Elem<int>() { Info = 6 },
+            //        Right = new Elem<int>() { Info = 7,
+            //            Left  = new Elem<int>() { Info = 8 },
+            //            Right = new Elem<int>() { Info = 9 }
+            //        }
+            //    }
+            //};
+
+            //t.ShowTree();
+            //Console.WriteLine();
+
+            //Tree t1 = new Tree();
+
+            //t1.Root = new Elem()
+            //{
+            //    Info = 1,
+            //    Left = new Elem()
+            //    {
+            //        Info = 2,
+            //        Left = new Elem() { Info = 4 },
+            //        Right = new Elem() { Info = 5 }
+            //    },
+            //    Right = new Elem()
+            //    {
+            //        Info = 3,
+            //        Left = new Elem() { Info = 6 },
+            //        Right = new Elem()
+            //        {
+            //            Info = 7,
+            //            Left = new Elem() { Info = 8 },
+            //            Right = new Elem() { Info = 9 }
+            //        }
+            //    }
+            //};
+
+            //Console.WriteLine($"Sum = {t1.SumTree()}");
+
+
+            String str = Console.ReadLine();
+
+            Tree t = new Tree();
+            t.CreateTree(str);
 
             t.ShowTree();
-            Console.WriteLine();
-
-            Tree t1 = new Tree();
-
-            t1.Root = new Elem()
-            {
-                Info = 1,
-                Left = new Elem()
-                {
-                    Info = 2,
-                    Left = new Elem() { Info = 4 },
-                    Right = new Elem() { Info = 5 }
-                },
-                Right = new Elem()
-                {
-                    Info = 3,
-                    Left = new Elem() { Info = 6 },
-                    Right = new Elem()
-                    {
-                        Info = 7,
-                        Left = new Elem() { Info = 8 },
-                        Right = new Elem() { Info = 9 }
-                    }
-                }
-            };
-
-            Console.WriteLine($"Sum = {t1.SumTree()}");
-
+            Console.WriteLine(t.SumTree());
 
 
         }
