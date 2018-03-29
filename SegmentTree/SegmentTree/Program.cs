99173a23f9bf1dd8ab323ece654f47db981639e1
moudroff <moudroff>
2018-03-29 13:09:21 +0300

    Дерево отрезков

---
+++ SegmentTree/SegmentTree/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SegmentTree
+{
+    class Elem
+    {
+        public readonly int LeftIndex; //{ get; private set; }
+        public readonly int RightIndex;// { get; private set; }
+        public int Info { get; set; }
+        public Elem Left { get; set; }
+        public Elem Right { get; set; }
+
+        public Elem(int li, int ri)
+        {
+            LeftIndex = li;
+            RightIndex = ri;
+        }
+
+    }
+    class SegmentTree
+    {
+        public Elem Root { get; set; }
+
+        private Elem CreateElem(int[] x, int li, int ri, Func<int, int, int> func)
+        {
+            Elem el = new Elem(li, ri);
+            if (li == ri)
+            {
+                el.Info = x[li];                
+            }
+            else
+            {
+                int mi = (li + ri) / 2;
+                el.Left = CreateElem(x, li, mi, func);
+                el.Right = CreateElem(x, mi + 1, ri, func);
+                el.Info = func(el.Right.Info,el.Left.Info);
+            }
+            return el;
+        }
+        public void CreateTree(int[] x, Func<int,int,int> func)
+        {
+            int n = x.Count();
+            Root = CreateElem(x, 0, n - 1, func);
+        }
+
+        private void ShowElem(Elem t)
+        {
+            if (t == null) return;
+            if (t.Left == t.Right)
+                Console.Write(t.Info);
+            else
+            {
+                Console.Write($"({t.Info},");
+                ShowElem(t.Left);
+                Console.Write(",");
+                ShowElem(t.Right);
+                Console.Write(")");
+            }
+        }
+
+        public void ShowTree()
+        {
+            ShowElem(Root);
+            Console.WriteLine();
+        }
+
+
+    }
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 15;
+            int[] x = new int[N];
+            for (int i = 0; i < N; i++)
+                x[i] = i;
+
+            SegmentTree t = new SegmentTree();
+            t.CreateTree(x, (a,b) => a+b);
+            t.ShowTree();
+
+        }
+    }
+}
---
+++ SegmentTree/SegmentTree/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("SegmentTree")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SegmentTree")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("42a3256b-1fc0-48cf-b9da-13c5c87466e4")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
